514ed7030a2241a2b947eaf17344f09a26ffb092
ssvtsfl <100675078+ssvtsfl@users.noreply.github.com>
2022-05-24 16:13:44 +0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -4,5 +4,7 @@
 class Queue : public List
 {
 public:
-    void push_front(int) override;
+    void push_front(int) override; //добавление элемента в начало списка
+    void print(std::ostream& out=std::cout);
 };
+
