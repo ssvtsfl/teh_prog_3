51f3f1a416d2e72dcf9326d8ff46d07c610344aa
ssvtsfl <100675078+ssvtsfl@users.noreply.github.com>
2022-05-24 16:11:46 +0300

    Update list.h

--- list.h
+++ list.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <iostream>
 class List
 {
 public:
@@ -9,13 +9,15 @@ public:
     void pop_front(); //удаление первого элемента в списке
     void push_back(int data); //добавление элемента в конец списка
     void clear(); // очистить список
-    int GetSize() { return Size; } // получить количество елементов в списке
-    int& operator[](const int index); // перегруженный оператор []
+    int GetSize() { return Size; } // получить количество элементов в списке
+    int& operator[](const int index); // перегруженный оператор []
     virtual void push_front(int data); //добавление элемента в начало списка
     void insert(int data, int index); //добавление элемента в список по указанному индексу
     void removeAt(int index); //удаление элемента в списке по указанному индексу
     void pop_back(); //удаление последнего элемента в списке
     virtual void get();
+    virtual void print(const char* delim=", ", std::ostream& out=std::cout);
+    int size() { return Size; };
 protected:
     class Node
     {
@@ -30,3 +32,4 @@ protected:
     int Size;
     Node* head;
 };
+
