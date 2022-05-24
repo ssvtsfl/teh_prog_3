7143762743625f800f256240e497df078fc8882b
ssvtsfl <100675078+ssvtsfl@users.noreply.github.com>
2022-05-24 16:14:58 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -4,6 +4,7 @@
 class Stack : public List
 {
 public:
-    void push_front(int) override;
+    void push_front(int) override; //добавление элемента в начало списка
     void get() override;
+    void print(std::ostream& out=std::cout);
 };
